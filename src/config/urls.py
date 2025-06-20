f34dd6319e4159806580a093a7da5550262cd1a8
joisemp <joise1124@gmail.com>
2025-06-20 22:47:33 +0530

    Added a landing page

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path('admin/', admin.site.urls),
     path('report/', include('reporthub.urls', namespace='reporthub')),
     path('core/', include('core.urls', namespace='core')),
