d58df666be780d0329a070d4531934b506f462fb
joisemp <joise1124@gmail.com>
2025-06-22 01:25:40 +0530

    updated url paths for issue_management

--- src/config/urls.py
+++ src/config/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
     path('admin/', admin.site.urls),
-    path('manage-issues/', include('issue_management.urls', namespace='issue_management')),
+    path('issue-management/', include('issue_management.urls', namespace='issue_management')),
     path('core/', include('core.urls', namespace='core')),
     path('dashboard/', include('dashboard.urls', namespace='dashboard')),
 ]
--- src/issue_management/urls.py
+++ src/issue_management/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'issue_management'
 
 urlpatterns = [
-    path('', views.issue_list, name='issue_list'),
-    path('report/', views.report_issue, name='report_issue'),
-    path('voice-record/', views.voice_record, name='voice_record'),
+    path('issues/', views.issue_list, name='issue_list'),
+    path('report-issue/', views.report_issue, name='report_issue'),
+    path('report-issue/voice-record/', views.voice_record, name='voice_record'),
 ]
