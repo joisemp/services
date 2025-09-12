d3e7356c171b5597906a45ae18279649b7908028
joisemp <joise1124@gmail.com>
2025-09-12 09:34:39 +0530

    feat: Enhance issue management with image upload functionality and media file serving

--- src/issue_management/forms.py
+++ src/issue_management/forms.py
@@ -3,6 +3,20 @@ from django import forms
 from .models import Issue
 
 class IssueForm(BootstrapFormMixin, forms.ModelForm):
+    # Add image fields for up to 3 images
+    image1 = forms.ImageField(required=False, widget=forms.FileInput(attrs={
+        'class': 'form-control',
+        'accept': 'image/*'
+    }))
+    image2 = forms.ImageField(required=False, widget=forms.FileInput(attrs={
+        'class': 'form-control',
+        'accept': 'image/*'
+    }))
+    image3 = forms.ImageField(required=False, widget=forms.FileInput(attrs={
+        'class': 'form-control',
+        'accept': 'image/*'
+    }))
+    
     class Meta:
         model = Issue
         fields = ['title', 'description', 'status', 'priority', 'org', 'space', 'voice']
--- src/issue_management/views/central_admin.py
+++ src/issue_management/views/central_admin.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from ..models import Issue
+from ..models import Issue, IssueImage
 from ..forms import IssueForm
 
 
@@ -10,10 +10,27 @@ class IssueListView(ListView):
     model = Issue
     
     def get_queryset(self):
-        return Issue.objects.all()
+        return Issue.objects.prefetch_related('images').select_related('org', 'space').all()
+
     
 class IssueCreateView(CreateView):
     template_name = "central_admin/issue_management/issue_create.html"
     form_class = IssueForm
     success_url = reverse_lazy('issue_management:central_admin:issue_list')
+    
+    def form_valid(self, form):
+        # Save the issue first
+        response = super().form_valid(form)
+        
+        # Handle image uploads
+        image_fields = ['image1', 'image2', 'image3']
+        for field_name in image_fields:
+            image_file = form.cleaned_data.get(field_name)
+            if image_file:
+                IssueImage.objects.create(
+                    issue=self.object,
+                    image=image_file
+                )
+        
+        return response
     
